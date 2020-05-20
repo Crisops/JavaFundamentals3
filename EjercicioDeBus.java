9f03271dfece31956cb766c35742eedf13108ab7
DavidLozano107 <53720016+DavidLozano107@users.noreply.github.com>
2020-05-19 23:35:07 -0500

    Update and rename JavaFundamentals.java to EjercicioDeBus.java

--- EjercicioDeBus.java
+++ EjercicioDeBus.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JavaFundamentals {
+public class EjercicioDeBus {
 
 	public static void main(String[] args) {
 		//Instanciamos la clase scanner para poder ingresar datos
